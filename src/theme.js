f18689e0e587ec80d0191879880c0b0373bcdb06
Kevin Gracia <kevin.gracia.tic@gmail.com>
2022-08-03 19:06:37 -0500

    feat: specific platform font

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,23 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
     textSecondary: '#586069',
     textWhite: 'white',
     primary: '#0366d6',
+    red: '#d73a4a',
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
   },
   fontWeights: {
     normal: '400',
